696de98393b9c491384c2423cbf776ac5d9af459
Anthony Bottemiller and jon Toler <jon@>
2016-08-29 10:23:57 -0700

    creating an object for a room with directions, and interactive options

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//BL
+function Room() {
+  this.narrative = "You enter a room. Dimly lit a fog is crawling in through the cracks on the west wall.";
+  this.northPassable = false;
+  this.southPassable = false;
+  this.westPassable = false;
+  this.eastPassable = false;
+  this.items = [];
+  this.creatures = [];
+}
