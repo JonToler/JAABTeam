3288d8b1c21d01c64d2e686dc378f18e25705a47
Ayana Powell and Angie Smith <ayana@>
2016-08-29 10:26:25 -0700

    Create an object person

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function User(name, gender) {
+  this.userName = name;
+  this.userGender = gender;
+  this.userStrength = 0;
+  this.userIntellect = 0;
+  this.userHealth = 10;
+  this.userPosition = [];
+  this.userInventory = [];
+};
+
+
+
+
+$(document).ready(function() {
+
+
+});
