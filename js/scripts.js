79ef9f0d81c2361883958a7649488b60b8c06d1e
Anthony Bottemiller and Angie Smith <angie@>
2016-08-30 11:33:45 -0700

    have room inventory functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function User(name, creatureType) {
   this.userName = name;
-  this.userChar = [];
+  this.userChar = creatureType;
   this.userStrength = 0;
   this.userIntellect = 0;
   this.userHealth = 10;
@@ -15,16 +15,21 @@ function Room() {
   this.southPassable = false;
   this.westPassable = false;
   this.eastPassable = false;
+  this.doors = [];
   this.items = [
-    new Item("lockpick", "intellect", "Pick up lockpick? It looks awfully rusty!"),
-    new Item("hammer", "strength", "Pick up hammer? Looks like it's been used a time or two!"),
-    new Item("book", "intellect", "Pick up book? Difficult, but you can read a few lines under the thick dust!")
+    new Item("lockpick", "intellect", "Select to pick up! It looks awfully rusty!"),
+    new Item("hammer", "strength", "Select to pick up! Looks like it's been used a time or two!"),
+    new Item("book", "intellect", "Select to pick up! Difficult, but you can read a few lines under the thick dust!")
   ];
   this.creatures = [
     new Creature("ghost" , 4, "OoOo a ghost has appeared! Cast a spell or fight it?"),
     new Creature("vampire", 3, "Watch out! A vampire is here!"),
     new Creature("dementors", 3, "Dementors are flying this way to suck your soul!")
   ];
+};
+
+function Door(){
+  this.options = ["pick lock", "use hammer", "barge in"];
 }
 
 function Item(itemName, itemTrait, itemNarrative) {
@@ -39,17 +44,18 @@ function Creature(creatureName, dmgOutput, creatureNarrative) {
   this.creatureNarrative = creatureNarrative;
 }
 
-Room.prototype.interact = function (user, item) {
+Room.prototype.interact = function(userInventory, item) {
   for (i=0; i < this.items.length; i++){
-    if (this.items[i] === item){
-      user.userInventory.push(this.items[i]);
+    if (this.items[i].itemName === item){
+      userInventory.push(this.items[i]);
+      console.log(userInventory);
       this.items.splice(i, 1);
-      showScore();
+      console.log(this.items);
     }
   }
 }
 
-Room.prototype.roomNarrative = function (user, narrative){
+Room.prototype.roomNarrative = function(user, narrative){
   return this.narrative;
 }
 
@@ -83,11 +89,20 @@ Creature.prototype.creatureDiceRoll = function(dmgOutput) {
   console.log()
 }
 
+User.prototype.addTrait = function(trait) {
+  if (trait === "intellect") {
+    this.userIntellect += 1;
+  } else if (trait === "strength") {
+    this.userStrength += 1;
+  }
+};
+
 $(document).ready(function() {
   var pass = false;
   var newUser;
   var diceRoll;
   var newItem;
+  var currentRoom;
 
   function showScore() {
     $('.this-health').text(newUser.userHealth);
@@ -96,28 +111,51 @@ $(document).ready(function() {
     $('.this-bag').append(newUser.userInventory);
   }
 
+  function roomInventory() {
+    for (i=0;i<currentRoom.items.length;i++) {
+      $("#room-inventory select").append("<option>" + i + " " + currentRoom.items[i].itemName + ": " + currentRoom.items[i].itemNarrative + "</option>");
+    };
+    $('#roomInventory').show();
+  };
+
   $('#begin-button').click(function(event) {
     event.preventDefault();
     var userName = $('#user-name').val();
     var userChar = $('#user-char').val();
     newUser = new User(userName, userChar);
-    newUser.userChar = newUser.userChar.push(userChar);
+    currentRoom = new Room();
     $('form#begin').fadeOut();
     setTimeout(function() {
       $('.this-name').append(newUser.userName);
-      $('.this-type').append(newUser.userChar[0]);
-      $('#game').append(newUser.userChar[1]);
+      $('.this-type').append(newUser.userChar);
+      $('.narrative p').append(currentRoom.narrative);
       showScore();
     $('#user-info').addClass('show');
     $('#game').addClass('show');
-    },900); 
+    },900);
   });
 
-  $('.option1').click(function() {
+  $('#search').click(function() {
+    roomInventory();
+  });
+
+  $("#room-inventory select").change(function(){
+    var itemSelected = ($("#room-inventory select option:selected").val()).substring(0,1);
+    alert("Congratulations! You have gained 1 " + currentRoom.items[itemSelected].itemTrait + "!");
+    newUser.addTrait(currentRoom.items[itemSelected].itemTrait);
+    currentRoom.interact(newUser.userInventory, currentRoom.items[itemSelected].itemName);
+    $("#room-inventory select").empty();
+    roomInventory();
+    showScore();
+  });
+
+  $('.option').show();
+  $('#option1').click(function() {
     newUser.addIntellect();
     showScore();
   });
-  $('.option2').click(function() {
+
+  $('#option2').click(function() {
     newUser.addStrength();
     showScore();
   });
